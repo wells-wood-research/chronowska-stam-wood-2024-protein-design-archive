03a9b2af49da6a2f453143965fef122c786fe386
Marta Chronowska <mchrnwsk@gmail.com>
2024-09-03 02:35:18 +0100

    Add Foldseek/DvD/chains

---
+++ Foldseek/DvD/chains/foldseek_search_dvd_single_chain.sh
@@ -0,0 +1,8 @@
+# Create query database (designed proteins, designed chains only)
+foldseek createdb 1450_designed_chain_structures ./data/queryDB --input-format 1  --chain-name-mode 0
+
+# Create target database (all of pdb)
+foldseek databases PDB ./data/targetDB-uncomp tmp
+
+# Perform search of structural similarity between target and query
+foldseek easy-search ./data/queryDB ./data/queryDB ./output/resultDB tmp/ -s 7.5 --exhaustive-search 1 --format-output query,qlen,target,tlen,prob,lddt,pident,cigar,qcov,tcov,alntmscore,lddt,evalue,bits
