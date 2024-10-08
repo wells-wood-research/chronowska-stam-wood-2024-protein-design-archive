df5e013a64d99fc0f75b80ed6db9b8e498c32ca8
Marta Chronowska <mchrnwsk@gmail.com>
2024-10-08 12:20:11 +0100

    Edit manual curation for 1mey to correct DNA and designed labels

--- manual_data_curation.py
+++ manual_data_curation.py
@@ -23,4 +23,15 @@ chains_5i9d[0][1]["chain_type"] = "N"
 data.loc[data["pdb"] == "5i9d", "chains"] = chains_5i9d
 data.at[index_5i9d, "review_comment"] = [["2024-10-06: Confirm chain A to be de novo designed rather than unknown", "2024-10-06: Confirm chain B to be RNA rather than de novo designed"]]
 
+index_1mey = data.loc[data["pdb"] == "1mey"].index[0]
+chains_1mey = data[data["pdb"] == "1mey"]["chains"].values
+chains_1mey[0][0]["chain_source"] = "DNA"
+chains_1mey[0][0]["chain_type"] = "N"
+chains_1mey[0][1]["chain_source"] = "DNA"
+chains_1mey[0][1]["chain_type"] = "N"
+chains_1mey[0][2]["chain_source"] = "synthetic construct"
+chains_1mey[0][2]["chain_type"] = "D"
+data.loc[data["pdb"] == "1mey", "chains"] = chains_1mey
+data.at[index_1mey, "review_comment"] = [["2024-10-07: Confirm chains A, D, B, E to be DNA rather than unknown", "2024-10-07: Confirm chain C, F, G to be de novo designed rather than unknown"]]
+
 data_result = data.to_json(filename_dataset+"_curated.json", orient="records", indent=4)
