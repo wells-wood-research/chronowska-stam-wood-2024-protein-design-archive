ed467130e453dc22486026a3c3712b651d0b2381
Marta Chronowska <mchrnwsk@gmail.com>
2024-10-10 14:42:30 +0100

    Adjust similarity analysis scripts to allow finding max similarity values not only above set threshold

--- scripts/similarity_analysis_foldseek.py
+++ scripts/similarity_analysis_foldseek.py
@@ -8,23 +8,25 @@ import statistics
 # Setup argument parser
 parser = argparse.ArgumentParser(description="Process Foldseek analysis")
 parser.add_argument("-d", "--date", type=str, required=True, help="Update date in iso format (20240930)")
-parser.add_argument("-a", "--analysis_type", type=str, required=True, help="Analysis type (DvD or DvP)")
+parser.add_argument("-a", "--thr_or_max", type=str, required=True, help="Find metric values above threshold or highest per design (\"thr\" or \"max\")")
+parser.add_argument("-p", "--prot_or_des", type=str, required=True, help="Compare designs against designs or natural proteins (\"DvD\" or \"DvP\")")
 parser.add_argument("-m", "--metric", default="lddt", type=str, required=True, help="Metric to use (lddt)")
 parser.add_argument("-t", "--threshold", default=0.9, type=float, required=True, help="Threshold for metric similarity (0.9)")
 args = parser.parse_args()
 
 # Assign command-line arguments to variables
 update_date = args.date
-analysis_type = args.analysis_type
+thr_or_max = args.thr_or_max
+prot_or_des = args.prot_or_des
 metric = args.metric
 threshold = args.threshold
 
 # Define paths for Foldseek output files
 base_dir = "/home/mchrnwsk/pda/foldseek"
 base_dir_current = base_dir+"/"+update_date
-current_dir_foldseek = base_dir_current+"/"+analysis_type
+current_dir_foldseek = base_dir_current+"/"+prot_or_des
 current_dir_foldseek_analysis = current_dir_foldseek+"/"+"analysis"
-output_file = base_dir_current+"/"+analysis_type+"_related_structure"+"_"+str(metric)+"_"+str(round(threshold*100))
+output_file = base_dir_current+"/"+prot_or_des+"_"+thr_or_max+"_related_structure"+"_"+str(metric)+"_"+str(round(threshold*100))
 
 # Load data
 data = pd.read_json(current_dir_foldseek_analysis+"/"+update_date+"_data.json")
@@ -34,7 +36,7 @@ pdb_release = pd.read_csv(current_dir_foldseek_analysis+"/"+"all_pdb_release_dat
 data_dict = dict(zip(data["pdb"].str.lower(), data["release_date"]))
 results = pd.read_csv(current_dir_foldseek+"/"+"output/resultDB", sep="\t")
 pdb_release_dict = dict(zip(pdb_release["Entry ID"].str.lower(), pdb_release["Release Date"]))
-if analysis_type != "DvD":
+if prot_or_des != "DvD":
     excluded_pdbs = set(data["pdb"].str.lower())
 metric_dict = {}
 
@@ -46,7 +48,7 @@ for chunk in pd.read_csv(current_dir_foldseek+"/"+"output/resultDB", sep="\t", h
 
     for i, row in chunk.iterrows():
         if i % 1000 == 0:
-            print(f"{analysis_type}: {i}/{len(results)}")
+            print(f"{prot_or_des}: {i}/{len(results)}")
         
         query_name = row["query"][:4].lower()
         target_name = row["target"][:4].lower()
@@ -54,7 +56,7 @@ for chunk in pd.read_csv(current_dir_foldseek+"/"+"output/resultDB", sep="\t", h
         if query_name == target_name:
             continue
 
-        if analysis_type != "DvD":
+        if prot_or_des != "DvD":
             if target_name in excluded_pdbs:
                 continue
 
@@ -65,28 +67,32 @@ for chunk in pd.read_csv(current_dir_foldseek+"/"+"output/resultDB", sep="\t", h
             print(f"Error at query: {query_name}. Missing release date for target: {target_name}")
             continue
 
-        if analysis_type != "DvD":
+        if prot_or_des != "DvD":
             if query_release < target_release:
                 continue
         
         similarity = row[metric]
         
-        if similarity >= threshold:
-            # Check if query_name is already in metric_dict
-            if query_name not in metric_dict:
-                # Add new entry for query_name
-                metric_dict[query_name] = [{"sim": similarity, "partner": target_name}]
-            else:
-                # Check if target_name already exists
-                existing_entry = next((entry for entry in metric_dict[query_name] if entry["partner"] == target_name), None)
-                
-                if existing_entry:
-                    # Compare and keep the higher similarity
-                    if similarity > existing_entry["sim"]:
-                        existing_entry["sim"] = similarity
+        if thr_or_max == "thr":
+            if similarity >= threshold:
+                if query_name not in metric_dict:
+                    metric_dict[query_name] = [{"sim": similarity, "partner": target_name}]
                 else:
-                    # Add the new target_name with its similarity
-                    metric_dict[query_name].append({"sim": similarity, "partner": target_name})
+                    existing_entry = next((entry for entry in metric_dict[query_name] if entry["partner"] == target_name), None)
+                    
+                    if existing_entry:
+                        if similarity > existing_entry["sim"]:
+                            existing_entry["sim"] = similarity
+                    else:
+                        metric_dict[query_name].append({"sim": similarity, "partner": target_name})
+        else:
+            if query_name not in metric_dict:
+                metric_dict[query_name] = {"sim": similarity, "partner": target_name}
+            else:
+                if metric_dict[query_name]["sim"] < similarity:
+                    metric_dict[query_name] = {"sim": similarity, "partner": target_name}
+                else:
+                    continue
 
     del chunk
     gc.collect()
@@ -98,53 +104,54 @@ with open(output_file+".json", "w") as outfile:
 print("")
 print("Output saved to: ", output_file+".json")
 
-# Calculate statistics
-lengths = [len(entry) for entry in metric_dict.values()]
-max_length = max(lengths)
-min_length = min(lengths)
-mean_length = sum(lengths) / len(lengths)
-median_length = statistics.median(lengths)
+if thr_or_max == "thr":
+    # Calculate statistics
+    lengths = [len(entry) for entry in metric_dict.values()]
+    max_length = max(lengths)
+    min_length = min(lengths)
+    mean_length = sum(lengths) / len(lengths)
+    median_length = statistics.median(lengths)
 
-# Print statistics
-print("")
-print(f"Max: {max(lengths)}, min: {min(lengths)}")
-print(f"Mean: {round(sum(lengths) / len(lengths))}, median: {round(statistics.median(lengths))}")
+    # Print statistics
+    print("")
+    print(f"Max: {max(lengths)}, min: {min(lengths)}")
+    print(f"Mean: {round(sum(lengths) / len(lengths))}, median: {round(statistics.median(lengths))}")
 
-# Sort lengths and count the top and bottom values
-lengths_dict = {}
-for v in metric_dict.values():
-    key = str(len(v))
-    if key not in lengths_dict:
-        lengths_dict[key] = 1
-    else:
-        lengths_dict[key] += 1
+    # Sort lengths and count the top and bottom values
+    lengths_dict = {}
+    for v in metric_dict.values():
+        key = str(len(v))
+        if key not in lengths_dict:
+            lengths_dict[key] = 1
+        else:
+            lengths_dict[key] += 1
 
-sorted_lengths = sorted(lengths, reverse=True)
+    sorted_lengths = sorted(lengths, reverse=True)
 
-sorted_dict = sorted(lengths_dict, key=lengths_dict.get, reverse=False)
+    sorted_dict = sorted(lengths_dict, key=lengths_dict.get, reverse=False)
 
-print("")
-for key in sorted_dict[:5]:
-    print(f"{lengths_dict[key]} designs have {key} related structures")
-print("")
-for key in sorted_dict[-5:]:
-    print(f"{lengths_dict[key]} designs have {key} related structures")
+    print("")
+    for key in sorted_dict[:5]:
+        print(f"{lengths_dict[key]} designs have {key} related structures")
+    print("")
+    for key in sorted_dict[-5:]:
+        print(f"{lengths_dict[key]} designs have {key} related structures")
 
-# Draw distribution graph
-import matplotlib
-matplotlib.use('Agg')  # Use a non-interactive backend
-import matplotlib.pyplot as plt
+    # Draw distribution graph
+    import matplotlib
+    matplotlib.use('Agg')  # Use a non-interactive backend
+    import matplotlib.pyplot as plt
 
-bins_range = 150
-bins = range(0, bins_range, 1)
-plt.rcParams["font.family"] = "monospace"
-fig = plt.figure(figsize=(12, 3))
-plt.title("Distribution of number of related structure of de novo designed chains", fontsize=14)
-plt.hist(lengths, bins=bins, range=(0, bins_range))
-plt.grid()
-plt.xlabel("Number of related structures", fontsize=14)
-plt.ylabel("Number of designs", fontsize=14)
-plt.xticks(range(0, bins_range+5, 5), rotation=90, fontsize=14)
-plt.yticks(fontsize=14)
-plt.xlim(0, bins_range)
-plt.savefig(output_file+".svg", bbox_inches='tight')
+    bins_range = 150
+    bins = range(0, bins_range, 1)
+    plt.rcParams["font.family"] = "monospace"
+    fig = plt.figure(figsize=(12, 3))
+    plt.title("Distribution of number of related structure of de novo designed chains", fontsize=14)
+    plt.hist(lengths, bins=bins, range=(0, bins_range))
+    plt.grid()
+    plt.xlabel("Number of related structures", fontsize=14)
+    plt.ylabel("Number of designs", fontsize=14)
+    plt.xticks(range(0, bins_range+5, 5), rotation=90, fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.xlim(0, bins_range)
+    plt.savefig(output_file+".svg", bbox_inches='tight')
--- scripts/similarity_analysis_mmseq.py
+++ scripts/similarity_analysis_mmseq.py
@@ -9,14 +9,16 @@ import math
 # Setup argument parser
 parser = argparse.ArgumentParser(description="Process MMseqs2 analysis")
 parser.add_argument("-d", "--date", type=str, required=True, help="Update date in iso format (20240930)")
-parser.add_argument("-a", "--analysis_type", type=str, required=True, help="Analysis type (DvD or DvP)")
+parser.add_argument("-a", "--thr_or_max", type=str, required=True, help="Find metric values above threshold or highest per design (\"thr\" or \"max\")")
+parser.add_argument("-p", "--prot_or_des", type=str, required=True, help="Compare designs against designs or natural proteins (\"DvD\" or \"DvP\")")
 parser.add_argument("-m", "--metric", default="bits", type=str, required=True, help="Metric to use (bits)")
 parser.add_argument("-t", "--threshold", default=50, type=float, required=True, help="Threshold for metric similarity (50)")
 args = parser.parse_args()
 
 # Assign command-line arguments to variables
 update_date = args.date
-analysis_type = args.analysis_type
+thr_or_max = args.thr_or_max
+prot_or_des = args.prot_or_des
 metric = args.metric
 threshold = args.threshold
 
@@ -24,8 +26,8 @@ threshold = args.threshold
 base_dir_data = "/home/mchrnwsk/pda-destress-analysis/data"
 base_dir_analysis = "/home/mchrnwsk/pda-destress-analysis/similarity/mmseq"
 current_dir_mmseq = base_dir_analysis+"/"+update_date
-current_dir_mmseq_analysis = base_dir_analysis+"/"+update_date+"/"+analysis_type
-output_file = current_dir_mmseq+"/"+analysis_type+"_related_sequence_"+str(metric)+"_"+str(round(threshold))
+current_dir_mmseq_analysis = base_dir_analysis+"/"+update_date+"/"+prot_or_des
+output_file = current_dir_mmseq+"/"+prot_or_des+"_"+thr_or_max+"_related_sequence"+"_"+str(metric)+"_"+str(round(threshold))
 
 # Load data
 data = pd.read_json(base_dir_data+"/"+update_date+"_data.json")
@@ -35,7 +37,7 @@ pdb_release = pd.read_csv(base_dir_data+"/"+"all_pdb_release_dates.csv")
 # Preprocess data into dictionaries for faster lookup
 data_dict = dict(zip(data["pdb"].str.lower(), data["release_date"]))
 pdb_release_dict = dict(zip(pdb_release["Entry ID"].str.lower(), pdb_release["Release Date"]))
-if analysis_type != "DvD":
+if prot_or_des != "DvD":
     excluded_pdbs = set(data["pdb"].str.lower())
 metric_dict = {}
 
@@ -46,7 +48,7 @@ for chunk in pd.read_csv(current_dir_mmseq_analysis+"/"+"results.m8", sep="\t", 
     
     for i, row in chunk.iterrows():
         if i % 1000 == 0:
-            print(f"{analysis_type}: {i}/{len(results)}")
+            print(f"{prot_or_des}: {i}/{len(results)}")
         
         query_name = row["query"][:4].lower()
         target_name = row["target"][:4].lower()
@@ -54,7 +56,7 @@ for chunk in pd.read_csv(current_dir_mmseq_analysis+"/"+"results.m8", sep="\t", 
         if query_name == target_name:
             continue
 
-        if analysis_type != "DvD":
+        if prot_or_des != "DvD":
             if target_name in excluded_pdbs:
                 continue
 
@@ -65,28 +67,32 @@ for chunk in pd.read_csv(current_dir_mmseq_analysis+"/"+"results.m8", sep="\t", 
             print(f"Error at query: {query_name}. Missing release date for target: {target_name}")
             continue
 
-        if analysis_type != "DvD":
+        if prot_or_des != "DvD":
             if query_release < target_release:
                 continue
         
         similarity = row[metric]
         
-        if similarity >= threshold:
-            # Check if query_name is already in metric_dict
-            if query_name not in metric_dict:
-                # Add new entry for query_name
-                metric_dict[query_name] = [{"sim": similarity, "partner": target_name}]
-            else:
-                # Check if target_name already exists
-                existing_entry = next((entry for entry in metric_dict[query_name] if entry["partner"] == target_name), None)
-                
-                if existing_entry:
-                    # Compare and keep the higher similarity
-                    if similarity > existing_entry["sim"]:
-                        existing_entry["sim"] = similarity
+        if thr_or_max == "thr":
+            if similarity >= threshold:
+                if query_name not in metric_dict:
+                    metric_dict[query_name] = [{"sim": similarity, "partner": target_name}]
                 else:
-                    # Add the new target_name with its similarity
-                    metric_dict[query_name].append({"sim": similarity, "partner": target_name})
+                    existing_entry = next((entry for entry in metric_dict[query_name] if entry["partner"] == target_name), None)
+                    
+                    if existing_entry:
+                        if similarity > existing_entry["sim"]:
+                            existing_entry["sim"] = similarity
+                    else:
+                        metric_dict[query_name].append({"sim": similarity, "partner": target_name})
+        else:
+            if query_name not in metric_dict:
+                metric_dict[query_name] = {"sim": similarity, "partner": target_name}
+            else:
+                if metric_dict[query_name]["sim"] < similarity:
+                    metric_dict[query_name] = {"sim": similarity, "partner": target_name}
+                else:
+                    continue
 
     del chunk
     gc.collect()
@@ -98,56 +104,57 @@ with open(output_file+".json", "w") as outfile:
 print("")
 print("Output saved to: ", output_file+".json")
 
-# Calculate statistics
-lengths = [len(entry) for entry in metric_dict.values()]
-max_length = max(lengths)
-min_length = min(lengths)
-mean_length = sum(lengths) / len(lengths)
-median_length = statistics.median(lengths)
+if thr_or_max == "thr":
+    # Calculate statistics
+    lengths = [len(entry) for entry in metric_dict.values()]
+    max_length = max(lengths)
+    min_length = min(lengths)
+    mean_length = sum(lengths) / len(lengths)
+    median_length = statistics.median(lengths)
 
-# Print statistics
-print("")
-print(f"Max: {max(lengths)}, min: {min(lengths)}")
-print(f"Mean: {round(sum(lengths) / len(lengths))}, median: {round(statistics.median(lengths))}")
+    # Print statistics
+    print("")
+    print(f"Max: {max(lengths)}, min: {min(lengths)}")
+    print(f"Mean: {round(sum(lengths) / len(lengths))}, median: {round(statistics.median(lengths))}")
 
-# Sort lengths and count the top and bottom values
-lengths_dict = {}
-for v in metric_dict.values():
-    key = str(len(v))
-    if key not in lengths_dict:
-        lengths_dict[key] = 1
-    else:
-        lengths_dict[key] += 1
+    # Sort lengths and count the top and bottom values
+    lengths_dict = {}
+    for v in metric_dict.values():
+        key = str(len(v))
+        if key not in lengths_dict:
+            lengths_dict[key] = 1
+        else:
+            lengths_dict[key] += 1
 
-sorted_lengths = sorted(lengths, reverse=True)
+    sorted_lengths = sorted(lengths, reverse=True)
 
-sorted_dict = sorted(lengths_dict, key=lengths_dict.get, reverse=False)
+    sorted_dict = sorted(lengths_dict, key=lengths_dict.get, reverse=False)
 
-print("")
-for key in sorted_dict[:5]:
-    print(f"{lengths_dict[key]} designs have {key} related sequences")
-print("")
-for key in sorted_dict[-5:]:
-    print(f"{lengths_dict[key]} designs have {key} related sequences")
+    print("")
+    for key in sorted_dict[:5]:
+        print(f"{lengths_dict[key]} designs have {key} related sequences")
+    print("")
+    for key in sorted_dict[-5:]:
+        print(f"{lengths_dict[key]} designs have {key} related sequences")
 
-# Draw distribution graph
-import matplotlib
-matplotlib.use('Agg')  # Use a non-interactive backend
-import matplotlib.pyplot as plt
+    # Draw distribution graph
+    import matplotlib
+    matplotlib.use('Agg')  # Use a non-interactive backend
+    import matplotlib.pyplot as plt
 
 
-bin_size = round(math.sqrt(max_length))  # Use larger bins for larger ranges
-bins = range(0, max_length + bin_size, bin_size)
-plt.rcParams["font.family"] = "monospace"
-fig = plt.figure(figsize=(12, 3))
-plt.title("Distribution of number of related sequences of de novo designed chains", fontsize=14)
-plt.hist(lengths, bins=bins, range=(0, max_length))
-plt.grid()
-plt.xlabel("Number of related sequences", fontsize=14)
-plt.ylabel("Number of designs", fontsize=14)
-xticks_range = range(0, max_length + bin_size, max(1, bin_size * 5))  # Set x-ticks based on bin size
-plt.xticks(xticks_range, rotation=90, fontsize=14)
-plt.yticks(fontsize=14)
-plt.xlim(0, max_length)
-plt.xlim(0, max(lengths))
-plt.savefig(output_file+".svg", bbox_inches='tight')
+    bin_size = round(math.sqrt(max_length))  # Use larger bins for larger ranges
+    bins = range(0, max_length + bin_size, bin_size)
+    plt.rcParams["font.family"] = "monospace"
+    fig = plt.figure(figsize=(12, 3))
+    plt.title("Distribution of number of related sequences of de novo designed chains", fontsize=14)
+    plt.hist(lengths, bins=bins, range=(0, max_length))
+    plt.grid()
+    plt.xlabel("Number of related sequences", fontsize=14)
+    plt.ylabel("Number of designs", fontsize=14)
+    xticks_range = range(0, max_length + bin_size, max(1, bin_size * 5))  # Set x-ticks based on bin size
+    plt.xticks(xticks_range, rotation=90, fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.xlim(0, max_length)
+    plt.xlim(0, max(lengths))
+    plt.savefig(output_file+".svg", bbox_inches='tight')
