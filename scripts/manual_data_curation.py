597bfd6e61fd186399cdedf81f08e877ff5567e8
Marta Chronowska <mchrnwsk@gmail.com>
2024-10-11 08:52:02 +0100

    Fix consistency between "chain" and "chains" in manual data curation comments

--- scripts/manual_data_curation.py
+++ scripts/manual_data_curation.py
@@ -27,7 +27,7 @@ chains_7jzl = data[data["pdb"] == "7jzl"]["chains"].values
 chains_7jzl[0][1]["chain_source"] = "synthetic construct"
 chains_7jzl[0][1]["chain_type"] = "D"
 data.loc[data["pdb"] == "7jzl", "chains"] = chains_7jzl
-data.at[index_7jzl, "review_comment"] = flatten_list(["2024-10-06: Confirm chain E,G,F to be de novo designed rather than unknown"])
+data.at[index_7jzl, "review_comment"] = flatten_list(["2024-10-06: Confirm chains E,G,F to be de novo designed rather than unknown"])
 
 index_5i9d = data.loc[data["pdb"] == "5i9d"].index[0]
 chains_5i9d = data[data["pdb"] == "5i9d"]["chains"].values
@@ -47,6 +47,6 @@ chains_1mey[0][1]["chain_type"] = "N"
 chains_1mey[0][2]["chain_source"] = "synthetic construct"
 chains_1mey[0][2]["chain_type"] = "D"
 data.loc[data["pdb"] == "1mey", "chains"] = chains_1mey
-data.at[index_1mey, "review_comment"] = flatten_list([["2024-10-07: Confirm chains A, D, B, E to be DNA rather than unknown", "2024-10-07: Confirm chain C, F, G to be de novo designed rather than unknown"]])
+data.at[index_1mey, "review_comment"] = flatten_list([["2024-10-07: Confirm chains A, D, B, E to be DNA rather than unknown", "2024-10-07: Confirm chains C, F, G to be de novo designed rather than unknown"]])
 
 data_result = data.to_json(base_dir_data+"/"+update_date+"_data_"+output_type+".json", orient="records", indent=4)
