371b50fc7147899941113d2675171ee1f795e76c
Marta Chronowska <mchrnwsk@gmail.com>
2024-11-12 16:45:20 +0000

    Fix paths in foldseek search script to work as part of automated workflow

--- Foldseek/foldseek_search_dvd.sh
+++ Foldseek/foldseek_search_dvd.sh
@@ -1,5 +1,5 @@
 # Create query database (designed proteins, designed chains only)
-foldseek createdb pdb_files_chains ./data/queryDB --input-format 1  --chain-name-mode 0
+foldseek createdb ./pdb_files_chains ./data/queryDB --input-format 1  --chain-name-mode 0
 
 # Perform search of structural similarity between target and query
 foldseek easy-search ./data/queryDB ./data/queryDB ./output/resultDB tmp/ -s 7.5 --exhaustive-search 1 --format-output query,qlen,target,tlen,prob,lddt,pident,cigar,qcov,tcov,alntmscore,lddt,evalue,bits
