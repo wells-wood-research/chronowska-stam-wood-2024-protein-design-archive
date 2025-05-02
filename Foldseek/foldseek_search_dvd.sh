b6db0398ba0074cb50448a181f2bb55e4fa64f26
Marta Chronowska <mchrnwsk@gmail.com>
2025-05-02 08:39:58 +0100

    Add self matches to determine better uncomputed similarity

--- Foldseek/foldseek_search_dvd.sh
+++ Foldseek/foldseek_search_dvd.sh
@@ -2,4 +2,4 @@
 foldseek createdb ./pdb_files_chains ./data/queryDB --input-format 1  --chain-name-mode 0
 
 # Perform search of structural similarity between target and query
-foldseek easy-search ./data/queryDB ./data/queryDB ./output/resultDB tmp/ -s 7.5 --exhaustive-search 1 --format-output query,qlen,target,tlen,prob,lddt,pident,cigar,qcov,tcov,alntmscore,lddt,evalue,bits
+foldseek easy-search ./data/queryDB ./data/queryDB ./output/resultDB tmp/ -s 7.5 --exhaustive-search 1 --add-self-matches --format-output query,qlen,target,tlen,prob,lddt,pident,cigar,qcov,tcov,alntmscore,lddt,evalue,bits
--- MMseqs2/mmseqs2_search_dvd.sh
+++ MMseqs2/mmseqs2_search_dvd.sh
@@ -4,4 +4,4 @@
 BASE_DIR=$(pwd)
 
 # Easy-search
-docker run -it --rm -v ${BASE_DIR}:/data soedinglab/mmseqs2 mmseqs easy-search /data/designed_single_sequences_1450.fasta /data/designed_single_sequences_1450.fasta /data/results.m8 /data/tmp -a 1 --exhaustive-search 1 -s 7.0 --format-output "query,qlen,target,tlen,bits,pident,raw,cigar,qcov,tcov"
+docker run -it --rm -v ${BASE_DIR}:/data soedinglab/mmseqs2 mmseqs easy-search /data/designed_single_sequences_1450.fasta /data/designed_single_sequences_1450.fasta /data/results.m8 /data/tmp -a 1 --exhaustive-search 1 -s 7.0 --add-self-matches --format-output "query,qlen,target,tlen,bits,pident,raw,cigar,qcov,tcov"
