71a3e7fac30eada60f14a5dfe71802f12010de91
Marta Chronowska <mchrnwsk@gmail.com>
2025-05-02 12:49:55 +0100

    Change scp host from cysteine (workstation failure) to glycine

--- scripts/prepare_foldseek_input.sh
+++ scripts/prepare_foldseek_input.sh
@@ -7,39 +7,37 @@ next_date="$1"
 analysis_type=("DvD" "DvP")
 
 # Start a persistent SSH connection
-ssh -Nf mchrnwsk@cysteine.bio.ed.ac.uk
+ssh -Nf mchrnwsk@glycine.bio.ed.ac.uk
 
 # Loop through each analysis type and create the necessary directories and copy files
 for type in "${analysis_type[@]}"; do
 
     mkdir -p /home/mchrnwsk/pda-destress-analysis/similarity/foldseek/$next_date/$type/
-
     # Ensure the destination directories exist
-    ssh mchrnwsk@cysteine.bio.ed.ac.uk "mkdir -p /home/mchrnwsk/pda/foldseek/$next_date/$type/analysis/"
-    ssh mchrnwsk@cysteine.bio.ed.ac.uk "mkdir -p /home/mchrnwsk/pda/foldseek/$next_date/$type/data/"
-    ssh mchrnwsk@cysteine.bio.ed.ac.uk "mkdir -p /home/mchrnwsk/pda/foldseek/$next_date/$type/output/"
-
+    ssh mchrnwsk@glycine.bio.ed.ac.uk "mkdir -p /home/mchrnwsk/pda/foldseek/$next_date/$type/analysis/"
+    ssh mchrnwsk@glycine.bio.ed.ac.uk "mkdir -p /home/mchrnwsk/pda/foldseek/$next_date/$type/data/"
+    ssh mchrnwsk@glycine.bio.ed.ac.uk "mkdir -p /home/mchrnwsk/pda/foldseek/$next_date/$type/output/"
     # Copy pdb_files_chains directory
     scp -r /home/mchrnwsk/pda-destress-analysis/data/pdb_files_chains \
-        mchrnwsk@cysteine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/$type/
+        mchrnwsk@glycine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/"$type"
 
     # Copy data.json to analysis directory
-    scp -r /home/mchrnwsk/pda-destress-analysis/data/"$next_date"_data.json \
-        mchrnwsk@cysteine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/"$type"/analysis
+    scp -r /home/mchrnwsk/pda-destress-analysis/data/"$next_date"_data_scraped.json \
+        mchrnwsk@glycine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/"$type"/analysis
 
     # Copy all_pdb_release_dates.csv to analysis directory
     scp -r /home/mchrnwsk/pda-destress-analysis/data/all_pdb_release_dates.csv \
-        mchrnwsk@cysteine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/"$type"/analysis
+        mchrnwsk@glycine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/"$type"/analysis
 
 done
 
 # Copy analysis script to analysis directory
 scp -r /home/mchrnwsk/pda-destress-analysis/similarity_analysis_foldseek.py \
-    mchrnwsk@cysteine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"
+    mchrnwsk@glycine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"
 
+# Copy Foldseek scripts
 scp -r /home/mchrnwsk/chronowska-stam-wood-2024-protein-design-archive/Foldseek/foldseek_search_dvd.sh \
-    mchrnwsk@cysteine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/DvD
+    mchrnwsk@glycine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/DvD
 
-# Copy foldseek_search_dvp.sh to the appropriate type directory
 scp -r /home/mchrnwsk/chronowska-stam-wood-2024-protein-design-archive/Foldseek/foldseek_search_dvp.sh \
-    mchrnwsk@cysteine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/DvP
+    mchrnwsk@glycine.bio.ed.ac.uk:/home/mchrnwsk/pda/foldseek/"$next_date"/DvP
